7afa7e8584d2ff58de65b6003a7a2e0aedd06b0c
Kirill Kapralov <Kapralov1501@gmail.com>
2025-02-03 23:32:13 +0200

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const path = require('path');
+const CopyPlugin = require("copy-webpack-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+
+module.exports = {
+  entry: './src/scripts/app.ts',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  plugins: [
+    new CopyPlugin({
+      patterns: [
+        { from: "src/styles", to: "styles" },
+        { from: "src/static", to: "static" },
+      ],
+    }),
+    new HtmlWebpackPlugin({
+      template: 'index.html',
+      inject: 'body',
+    }),
+  ],
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'public'),
+    },
+    compress: true,
+    port: 9000,
+  },
+  mode: 'development',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+};
+
